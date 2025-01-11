387bf4f669f47f906d6d400dca60403f074a9d53
Shresth72 <97455610+Shresth72@users.noreply.github.com>
2025-01-11 07:56:21 +0000

    without for loop macro

--- fasm/main.asm
+++ fasm/main.asm
@@ -4,20 +4,79 @@ format ELF64 executable
 ;;  1 - stdout
 ;;  2 - stderr
 
+hello: file "message.txt"
+hello_len = $-hello
+
+another: db "Goodbye!", 0xA
+another_len = $ - another
+
+print:
+    mov r9, -3689348814741910323
+    sub rsp, 40
+    mov BYTE [rsp+31], 10
+    lea rcx, [rsp+30]
+.L2:
+    mov rax, rdi
+    lea r8, [rsp+32]
+    mul r9
+    mov rax, rdi
+    sub r8, rcx
+    shr rdx, 3
+    lea rsi, [rdx+rdx*4]
+    add rsi, rsi
+    sub rax, rsi
+    add eax, 48
+    mov BYTE [rcx], al
+    mov rax, rdi
+    mov rdi, rdx
+    mov rdx, rcx
+    sub rcx, 1
+    cmp rax, 9
+    ja .L2
+    lea rax, [rsp+32]
+    mov edi, 1
+    sub rdx, rax
+    xor eax, eax
+    lea rsi, [rsp+32+rdx]
+    mov rdx, r8
+    mov rax, 1
+    syscall
+    add rsp, 40
+    ret
+
+macro for addr, again, over { ;; counter addr, labels..
+    mov QWORD [addr], 0
+again 
+    cmp QWORD [addr], 10
+    jge over
+}
+
+macro endfor addr, again, over {
+    inc QWORD [addr]
+    jmp again
+over:
+}
+
 entry _start
 _start:
-    ;; int3
-    mov r15, 10
+
+    ;; mov r15, 0
+    mov QWORD [i], 0
 
 .again:
-    cmp r15, 0
-    jle .over
-	    mov rax, 1
-  	    mov rdi, 1
-  	    mov rsi, hello
-  	    mov rdx, hello_len
-  	    syscall
-        dec r15
+    cmp QWORD [i], 10
+    jge .over
+        mov rsi, hello
+        mov rdx, hello_len
+        call print_str
+
+        lea rsi, [another] ;; Load Affective Address
+        mov rdx, another_len
+        call print_str
+
+        mov rdi, QWORD [i]
+        call print
+    inc QWORD [i]
     jmp .again
 
 .over:
@@ -25,6 +84,8 @@ _start:
     mov rdi, 0
     syscall
 
-;; "=" can be forward referenced
-hello: file "message.txt"
-hello_len = $-hello
+print_str:
+    mov rax, 1  ;; syscall number for write
+    mov rdi, 1  ;; file descriptor: stdout
+    syscall     ;; perform syscall
+    ret         ;; return to caller
